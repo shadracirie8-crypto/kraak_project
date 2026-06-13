08ed1b4aa99733980bc06f49892087474c57df08
shadracirie8-crypto <shadracirie8@gmail.com>
2026-06-13 22:45:20 +0000

    Remove cloudinary storage

--- kraak_project/settings.py
+++ kraak_project/settings.py
@@ -1,108 +1,161 @@
-
 import os
 from pathlib import Path
-import cloudinary
+
 from decouple import config
 import dj_database_url
 
 
-# Build paths inside the project like this: BASE_DIR / 'subdir'.
+# Build paths inside the project
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/6.0/howto/deployment/checklist/
-
-# SECURITY WARNING: keep the secret key used in production secret!
+# Security
 
 SECRET_KEY = config('SECRET_KEY')
+
 DEBUG = False
-ALLOWED_HOSTS = ['.onrender.com', '127.0.0.1', 'localhost']
+
+ALLOWED_HOSTS = [
+    '.onrender.com',
+    '127.0.0.1',
+    'localhost',
+]
+
 
 CSRF_TRUSTED_ORIGINS = [
     'https://*.onrender.com',
 ]
 
 
-
 # Application definition
 
 INSTALLED_APPS = [
+
+    # Django apps
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+
+    # Applications locales
     'core',
     'services',
     'programs',
     'contact',
     'accounts',
-    'cloudinary',
-    'cloudinary_storage',
 ]
 
+
 MIDDLEWARE = [
+
     'django.middleware.security.SecurityMiddleware',
+
+    # Static files Render
     'whitenoise.middleware.WhiteNoiseMiddleware',
+
     'django.contrib.sessions.middleware.SessionMiddleware',
+
     'django.middleware.common.CommonMiddleware',
+
     'django.middleware.csrf.CsrfViewMiddleware',
+
     'django.contrib.auth.middleware.AuthenticationMiddleware',
+
     'django.contrib.messages.middleware.MessageMiddleware',
+
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+
 ]
+
 
 ROOT_URLCONF = 'kraak_project.urls'
 
+
 TEMPLATES = [
+
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+
+        'DIRS': [
+            os.path.join(BASE_DIR, 'templates')
+        ],
+
         'APP_DIRS': True,
+
         'OPTIONS': {
+
             'context_processors': [
+
                 'django.template.context_processors.request',
+
                 'django.contrib.auth.context_processors.auth',
+
                 'django.contrib.messages.context_processors.messages',
+
             ],
+
         },
+
     },
+
 ]
+
 
 WSGI_APPLICATION = 'kraak_project.wsgi.application'
 
 
-# Database
-# https://docs.djangoproject.com/en/6.0/ref/settings/#databases
+
+# Database PostgreSQL Render
 
 DATABASES = {
-    'default': dj_database_url.parse(config('DATABASE_URL'))
+
+    'default': dj_database_url.config(
+
+        default=config('DATABASE_URL'),
+
+        conn_max_age=600
+
+    )
+
 }
 
 
+
 # Password validation
-# https://docs.djangoproject.com/en/6.0/ref/settings/#auth-password-validators
 
 AUTH_PASSWORD_VALIDATORS = [
+
     {
-        'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
+        'NAME':
+        'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
     },
+
+
     {
-        'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
+        'NAME':
+        'django.contrib.auth.password_validation.MinimumLengthValidator',
     },
+
+
     {
-        'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
+        'NAME':
+        'django.contrib.auth.password_validation.CommonPasswordValidator',
     },
+
+
     {
-        'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
+        'NAME':
+        'django.contrib.auth.password_validation.NumericPasswordValidator',
     },
+
 ]
 
 
+
 # Internationalization
-# https://docs.djangoproject.com/en/6.0/topics/i18n/
 
 LANGUAGE_CODE = 'en-us'
 
@@ -113,78 +166,70 @@ USE_I18N = True
 USE_TZ = True
 
 
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/6.0/howto/static-files/
+
+# Static files
+
+STATIC_URL = '/static/'
 
 
 STATICFILES_DIRS = [
+
     os.path.join(BASE_DIR, 'static')
+
 ]
 
-TEMPLATES[0]['DIRS'] = [os.path.join(BASE_DIR, 'templates')]
 
+STATIC_ROOT = BASE_DIR / 'staticfiles'
+
+
+STATICFILES_STORAGE = (
+
+    'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
+)
+
+
+
+# Media files (images, uploads)
+
+MEDIA_URL = '/media/'
 
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
-LOGIN_REDIRECT_URL = 'redirect_after_login' 
+
+
+# Authentication
+
+LOGIN_REDIRECT_URL = 'redirect_after_login'
+
 LOGOUT_REDIRECT_URL = 'home'
+
 LOGIN_URL = 'login'
+
 
 AUTH_USER_MODEL = 'accounts.User'
 
+
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-STATIC_URL = '/static/'
 
-STATIC_ROOT = BASE_DIR / 'staticfiles'
 
-MEDIA_URL = '/media/'
-MEDIA_ROOT = BASE_DIR / 'media'
-
-DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
-
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
-
-CLOUDINARY_STORAGE = {
-    'CLOUD_NAME': config('CLOUD_NAME'),
-    'API_KEY': config('API_KEY'),
-    'API_SECRET': config('API_SECRET'),
-}
+# Security production
 
 SECURE_SSL_REDIRECT = False
 
+
 SESSION_COOKIE_SECURE = True
+
 
 CSRF_COOKIE_SECURE = True
 
+
 SECURE_HSTS_SECONDS = 31536000
+
 
 SECURE_HSTS_INCLUDE_SUBDOMAINS = True
 
+
 SECURE_HSTS_PRELOAD = True
-
-CLOUDINARY_STORAGE = {
-    'CLOUD_NAME': config('CLOUD_NAME'),
-    'API_KEY': config('API_KEY'),
-    'API_SECRET': config('API_SECRET'),
-}
-
-
-
-
-
-cloudinary.config(
-    cloud_name=config("CLOUD_NAME"),
-    api_key=config("API_KEY"),
-    api_secret=config("API_SECRET"),
-)
-
-STORAGES = {
-    "default": {
-        "BACKEND": "cloudinary_storage.storage.MediaCloudinaryStorage",
-    },
-    "staticfiles": {
-        "BACKEND": "whitenoise.storage.CompressedManifestStaticFilesStorage",
-    },
-}
